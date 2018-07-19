9df40e7dba825e7109a7f452c50c48dcb38d0b28
CLdevops <41025631+CLdevops@users.noreply.github.com>
2018-07-19 09:46:03 -0400

    Create complete/env.rb
    CB-207 #comment Create env.rb #time 0w 0d 0h 10m

---
+++ complete/env.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby -w
+# These parameters will be exported elsewhere
+
+ENV = {
+  :shell  => ENV['SHELL'],
+  :user   => ENV['USER'],
+  :groups => ENV['GROUPS'],
+  :uid    => ENV['UID']
+}
+
+
