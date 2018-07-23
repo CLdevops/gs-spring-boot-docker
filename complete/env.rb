6ae46d375cd75e06966f21eb562423511c827a66
CLdevops <41025631+CLdevops@users.noreply.github.com>
2018-07-23 10:43:41 -0400

    Delete env.rb
    CB-207 #comment Remove env.rb file #time 0w 0d 0h 5m

--- complete/env.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# These parameters will be exported elsewhere
-
-ENV = {
-  :shell  => ENV['SHELL'],
-  :user   => ENV['USER'],
-  :groups => ENV['GROUPS'],
-  :uid    => ENV['UID']
-}
